6fbc7da4e47ff822d236d181abaa16acd1779e3c
Breno Ferreira <165968939+breno-hof@users.noreply.github.com>
2025-02-04 10:15:17 +0000

    debug openapi.yaml

--- src/locals.tf
+++ src/locals.tf
@@ -1,6 +1,6 @@
 locals {
 	name				= "pool-monitoring"
-	openapi_file_path	= "${path.module}/openapi.json"
+	openapi_file_path	= "${path.module}/openapi.yaml"
 
 	tags				= {
 		Application		= local.name,
--- src/main.tf
+++ src/main.tf
@@ -1,5 +1,5 @@
 module "apigateway_v2" {
-	source					= "../../module-api-gateway-rest-api/src"
+	source					= "github.com/breno-hof/module-api-gateway-rest-api//src?ref=1.0.0"
 
 	name					= "${local.name}-api-gateway-v2"
 	openapi_file_path		= local.openapi_file_path
@@ -13,21 +13,21 @@ module "apigateway_v2" {
 	description				= "My awesome HTTP API Gateway"
 	fail_on_warnings		= false
 
-	# authorizer = {
-	# 	authorizer_type		= "JWT"
-	# 	identity_sources	= ["$request.header.Authorization"]
-	# 	name				= "cognito"
-	# 	jwt_configuration	= {
-	# 		audience		= ["d6a38afd-45d6-4874-d1aa-3c5c558aqcc2"]
-	# 		issuer			= "https://${aws_cognito_user_pool.this.endpoint}"
-	# 	}
-	# }
+	authorizer = {
+		authorizer_type		= "JWT"
+		identity_sources	= ["$request.header.Authorization"]
+		name				= "cognito"
+		jwt_configuration	= {
+			audience		= [""]
+			issuer			= "https://${aws_cognito_user_pool.this.endpoint}"
+		}
+	}
 	
-	tags = local.tags
+	tags					= local.tags
 }
 
-# resource "aws_cognito_user_pool" "this" {
-#   name = "${local.name}-cognito-user-pool"
+resource "aws_cognito_user_pool" "this" {
+	name					= "${local.name}-cognito-user-pool"
 
-#   tags = local.tags
-# }
+	tags					= local.tags
+}
